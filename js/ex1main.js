d26fd347b81ca4ac0fd8351c995fe733d7f4542c
Daniel Maslov <maslov4real@gmail.com>
2023-02-03 15:34:14 +0200

    js update

--- js/ex1main.js
+++ js/ex1main.js
@@ -5,13 +5,20 @@ function onInit() {
 }
 
 function renderRobots(robots) {
-
-  const strHtml = robots.map(bot =>`
-    <div class="card">
-        <h1>${bot.fullname}</h1>
-    </div>`
-).join('')
-document.querySelector('.cards-main').innerHTML = strHtml
-
-
+    const strHtml = robots.map(bot =>`    
+        <div class="card">
+            <h1>${bot.fullname}</h1>
+            <div class="info">
+                <img src="https://robohash.org/${bot.fullname}" />
+                <p>
+                    <span> Phone: </span> ${bot.phone}, <br>
+                    <span> City: </span> ${bot.city}, <br>
+                    <span> State: </span> ${bot.state}, <br>
+                    <span> Zip: </span> ${bot.zip}, <br>
+                    <span> Address: </span> ${bot.address}. <br>
+                </p>
+            </div>
+        </div>`
+    ).join('')
+    document.querySelector('.cards-main').innerHTML = strHtml
 }
